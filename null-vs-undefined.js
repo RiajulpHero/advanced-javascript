7d1dd0ec9b8f84e6c384807cd2270db64eea07bd
RiajulpHero <67540508+RiajulpHero@users.noreply.github.com>
2020-08-14 13:01:04 +0600

    null-vs-undefined-exporing

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//      console.log(num1 + num2);
+// }
+// const result = add(23, 34);
+// console.log(result);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(23, 34);
+// console.log(result);
+
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(23);
+// console.log(result);
+
+// const premik = {name:"smart dude", phone:3487}
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages =[23, 45, 56];
+console.log(ages[11]);
