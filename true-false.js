c7c980328390ea78f8182f48fc6629d03bbe6235
RiajulpHero <67540508+RiajulpHero@users.noreply.github.com>
2020-08-14 12:38:30 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,125 @@
+
+const age = 4;
+if(age > 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false")
+}
+
+const age1 = -3;
+if(age1){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false")
+}
+
+const age2 = 5;
+if(age2){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false")
+}
+
+
+const age3 = 0;
+if(age3){
+    console.log("Condition is true for zero");
+}
+else{
+    console.log("Condition is false for zero");
+}
+
+const name = "Sulaiman";
+if(name.length > 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+const name1 = "Sulaiman";
+if(name1.length){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+const name2 = "Sulaiman";
+if(name2){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+const name3 = "";
+if(name3){
+    console.log("Condition is true for empty quotation");
+}
+else{
+    console.log("Condition is false for empty quotation");
+}
+
+
+let name4;
+console.log(name4);
+
+let name5 = null;
+if(name5){
+    console.log("condition is true for null");
+}
+else{
+    console.log("condition is false for null");
+}
+
+
+let name6 = NaN;
+if(name6){
+    console.log("condition is true for NaN");
+}
+else{
+    console.log("condition is false for NaN");
+}
+
+let name7 = [];
+if(name7){
+    console.log("condition is true for empty array");
+}
+else{
+    console.log("condition is false for empty array")
+}
+
+let name8 = {};
+if(name8){
+    console.log("condition is true for empty object");
+} 
+else{
+    console.log("condition is false for empty object");
+} 
+
+
+let name9 = false;
+if(name9){
+    console.log("condition is true for false");
+} 
+else{
+    console.log("condition is false for false");
+} 
+
+//falsy: these all are false
+//false
+//0
+//""               
+//undefined
+//NaN
+//null                
+//Truthy: these all are true
+//" ", "0" 
+//[], {}        
